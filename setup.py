627b55ae314e9978e8049850212ff730a61719a5
Peter Lisak <peter.lisak@gmail.com>
2015-03-21 20:31:45 +0100

    classifiers

--- setup.py
+++ setup.py
@@ -13,8 +13,21 @@ setup(name='py-romanify',
       author_email='peter.lisak+pypi@gmail.com',
       url='https://github.com/peter-lisak/py-romanify',
       packages=['romanify'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',	
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Topic :: Communications :: Email',
+		  'Topic :: Education',
+		  'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
       license="GNU GENERAL PUBLIC LICENSE"
      )
 
-# check long-desc
-#python setup.py --long-description | rst2html.py > output.html
